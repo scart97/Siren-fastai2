0b737a4e30d25f2c372d432788ee39e50f7d53b5
scart97 <scart.lucas@gmail.com>
2020-06-20 21:24:43 -0300

    Small refactoring at siren.py

--- siren.py
+++ siren.py
@@ -4,55 +4,58 @@ import torch.nn.functional as F
 from typing import List
 import math
 
+
 def siren_init(tensor, use_this_fan_in=None):
     if use_this_fan_in is not None:
         fan_in = use_this_fan_in
     else:
-        fan_in = nn.init._calculate_correct_fan(tensor, 'fan_in')
+        fan_in = nn.init._calculate_correct_fan(tensor, "fan_in")
     bound = math.sqrt(6.0 / fan_in)
     with torch.no_grad():
         return tensor.uniform_(-bound, bound)
 
 
-class SirenLayer(nn.Module):
+def apply_siren_init(layer: nn.Module):
+    siren_init(layer.weight)
+    if layer.bias is not None:
+        fan_in = nn.init._calculate_correct_fan(layer.weight, "fan_in")
+        siren_init(layer.bias, use_this_fan_in=fan_in)
+
+
+class Siren(nn.Module):
     """
-        Siren Layer - it's a modified linear layer with sine activation
+        Siren activation
         https://arxiv.org/abs/2006.09661
     """
-    def __init__(self, in_features, out_features, bias=True, w0 = 1):
+
+    def __init__(self, w0=1):
         """
-            w0 comes from the end of section 3, it should be 30 for the first layer
+            w0 comes from the end of section 3
+            it should be 30 for the first layer
+            and 1 for the rest
         """
         super().__init__()
-        self.in_features = in_features
-        self.out_features = out_features
-        self.weight = nn.Parameter(torch.Tensor(out_features, in_features))
-        if bias:
-            self.bias = nn.Parameter(torch.Tensor(out_features))
-        else:
-            self.register_parameter('bias', None)
         self.w0 = torch.tensor(w0)
-        self.reset_parameters()
-
-    def reset_parameters(self):
-        siren_init(self.weight)
-        if self.bias is not None:
-            fan_in = nn.init._calculate_correct_fan(self.weight, 'fan_in')
-            siren_init(self.bias, use_this_fan_in=fan_in)
 
     def forward(self, x):
-        x = F.linear(x, self.w0 * self.weight, self.bias)
-        return torch.sin(x)
-    
+        return torch.sin(self.w0 * x)
+
     def extra_repr(self):
-        return 'in_features={}, out_features={}, bias={}, w0={}'.format(
-            self.in_features, self.out_features, self.bias is not None, self.w0
-        )
+        return "w0={}".format(self.w0)
 
 
-def siren_model(dimensions:List[int]):
-    first_layer = SirenLayer(dimensions[0], dimensions[1], w0=30)
+def siren_layer(in_features, out_features, bias=True, w0=1):
+    """
+        Siren Layer - it's a modified linear layer with sine activation
+    """
+    layer = nn.Sequential(nn.Linear(in_features, out_features, bias), Siren(w0))
+    apply_siren_init(layer[0])
+    return layer
+
+
+def siren_model(dimensions: List[int]):
+    first_layer = siren_layer(dimensions[0], dimensions[1], w0=30)
     other_layers = []
     for dim0, dim1 in zip(dimensions[1:-1], dimensions[2:]):
-        other_layers.append(SirenLayer(dim0, dim1))
-    return nn.Sequential(first_layer, *other_layers)        
+        other_layers.append(siren_layer(dim0, dim1))
+    return nn.Sequential(first_layer, *other_layers)
